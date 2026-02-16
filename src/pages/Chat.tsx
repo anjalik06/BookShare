b1e8c431e6f4f80a99d54b98f4674484cf67cd34
anjalik06 <anjalik7007@gmail.com>
2026-02-16 19:18:50 +0530

    changes

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
 import { io, Socket } from "socket.io-client";
